24f1ee388c301762d692ed60302c5cfd55703907
WillDera <godswillezeoke@gmail.com>
2023-03-07 11:02:55 +0100

    Add more error messages

--- programs/solearn-contract/src/errors.rs
+++ programs/solearn-contract/src/errors.rs
@@ -2,8 +2,12 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum SolEarnError {
+    #[msg("Total sol and sum of sol allocation not equal.")]
+    IncorrectAllocations,
     #[msg("Insufficient SOL for payroll.")]
     InsufficientSol,
     #[msg("Number of employees dont match number of payout amounts.")]
     InvalidEmployeeAmountCount,
+    #[msg("Not Signer! Can't initiate transfer")]
+    NotSigner,
 }
