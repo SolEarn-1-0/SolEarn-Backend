0970cdd98d8a08536c0b2a0907fc57bb8d13bf10
WillDera <godswillezeoke@gmail.com>
2023-03-07 11:02:30 +0100

    Implement: Transfer sol from owner to PDA

--- programs/solearn-contract/src/lib.rs
+++ programs/solearn-contract/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+// use anchor_spl::token::{self, SetAuthority, TokenAccount, Transfer, InitializeAccount};
+use anchor_lang::{system_program::Transfer, system_program::transfer, solana_program::native_token::sol_to_lamports, solana_program::program::invoke_signed};
 
 use constants::*;
 use errors::*;
@@ -12,6 +14,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 mod solearn_contract {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
@@ -32,11 +35,29 @@ mod solearn_contract {
         let employees = &ctx.remaining_accounts;
 
         let total_amount: u64 = amounts.iter().sum();
-        require!(total_amount == sol_tokens, SolEarnError::InsufficientSol);
+        require!(total_amount == sol_tokens, SolEarnError::IncorrectAllocations);
         require!(
             employees.len() as u16 == amounts.len() as u16,
             SolEarnError::InvalidEmployeeAmountCount
         );
+        require!(ctx.accounts.employer.is_signer, SolEarnError::NotSigner);
+
+        let employer_lamports: u64 = ctx.accounts.employer.lamports();
+        require!(employer_lamports > sol_to_lamports(sol_tokens as f64), SolEarnError::InsufficientSol);
+        emit!(MyEvent{ data: employer_lamports });
+
+        let lmps: u64 = ctx.accounts.to.to_account_info().lamports();
+        emit!(MyEvent{ data: lmps });
+
+        let transfer_cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            Transfer {
+                from: ctx.accounts.employer.to_account_info(),
+                to: ctx.accounts.to.to_account_info()
+            },
+        );
+
+        transfer(transfer_cpi_context, sol_tokens).unwrap();
 
         employer_account.employee_count = employees.len() as u16;
 
@@ -44,6 +65,12 @@ mod solearn_contract {
             payroll_account.employee_addresses.push(employee.key());
             payroll_account.employee_salaries.push(amounts[i]);
         }
+        
+        let lmps_after: u64 = ctx.accounts.to.to_account_info().lamports();
+        emit!(MyEvent{ data: lmps_after });
+
+        let employer_lamports: u64 = ctx.accounts.employer.lamports();
+        emit!(MyEvent{ data: employer_lamports });
 
         Ok(())
     }
@@ -91,12 +118,25 @@ pub struct AddEmployees<'info> {
     )]
     pub payroll_account: Box<Account<'info, Payroll>>,
 
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub to: UncheckedAccount<'info>, 
+
     pub system_program: Program<'info, System>,
+    // pub token_program: Program<'info, TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct AssignAuthorityToPDA<'info> {
+    #[account(mut)]
+    current_authority_signer: AccountInfo<'info>,
+    #[account(mut)]
+    pub program_pubkey: AccountInfo<'info>,
 }
 
 #[event]
 pub struct MyEvent {
-    pub data: u16,
+    pub data: u64,
 }
 
 #[event]
@@ -112,4 +152,15 @@ pub struct MyEventTest {
 //     // pub employer: AccountInfo<'info>,
 //     #[account(mut)]
 //     pub system_program: Program<'info, System>,
+// }
+
+
+// impl<'info> AddEmployees<'info> {
+//     fn transfer_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+//         let cpi_accounts = Transfer {
+//             from: self.employer.to_account_info().clone(),
+//             to: self.to.to_account_info().clone(),
+//             authority: self.payroll_account.to_account_info().clone(),
+//         };
+//     }
 // }
