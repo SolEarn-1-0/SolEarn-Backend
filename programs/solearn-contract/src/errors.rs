f9ca397dce24387d2ae66cafb02694ab3dfd5d69
WillDera <godswillezeoke@gmail.com>
2023-03-10 03:19:05 +0100

    more error logs

--- programs/solearn-contract/src/errors.rs
+++ programs/solearn-contract/src/errors.rs
@@ -2,24 +2,30 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum SolEarnError {
-    #[msg("Total sol and sum of sol allocation not equal.")]
+    #[msg("Total sol and sum of sol allocation not equal!")]
     IncorrectAllocations,
 
-    #[msg("Insufficient SOL for payroll.")]
+    #[msg("Insufficient SOL for payroll!")]
     InsufficientSol,
 
-    #[msg("Number of employees dont match number of payout amounts.")]
+    #[msg("Number of employees dont match number of payout amounts!")]
     InvalidEmployeeAmountCount,
 
-    #[msg("Number of employees in Organization dont match number of payout accounts.")]
+    #[msg("Number of employees in Organization don't match number of payout accounts!")]
     InvalidEmployeePayrollCount,
 
-    #[msg("Pubkey not a member of this Payroll.")]
+    #[msg("Pubkey not a member of this Payroll!")]
     NotMember,
 
-    #[msg("Not Signer! Can't initiate transfer")]
+    #[msg("Not Signer! Can't initiate transfer.")]
     NotSigner,
 
-    #[msg("Unauthorized: Employer key mismatch")]
+    #[msg("Unauthorized: Employer key mismatch!")]
     Unauthorized,
+
+    #[msg("Salary already claimed!")]
+    SalararyClaim,
+
+    #[msg("All Employees on this Payroll have been paid!")]
+    PayrollInactive,
 }
