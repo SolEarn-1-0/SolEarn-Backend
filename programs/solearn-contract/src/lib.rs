1e74b6a5df485bcecbe53b37c1065fc4146fc87b
WillDera <godswillezeoke@gmail.com>
2023-03-10 03:19:35 +0100

    payout logic working

--- programs/solearn-contract/src/lib.rs
+++ programs/solearn-contract/src/lib.rs
@@ -24,54 +24,71 @@ mod solearn_contract {
 
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let employer_account = &mut ctx.accounts.organization_account;
-        employer_account.employer = ctx.accounts.employer.key();
-        employer_account.employee_count = 0;
-
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
-    pub fn add_employees(
-        ctx: Context<AddEmployees>,
-        amounts: Vec<u64>,
-        sol_tokens: u64,
-    ) -> Result<()> {
-        let employer_account = &mut ctx.accounts.organization_account;
-        let payroll_account = &mut ctx.accounts.payroll_account;
-        payroll_account.employer = ctx.accounts.employer.key();
-        let employees = &ctx.remaining_accounts;
+    pub fn create_organization(ctx: Context<CreateOrganzation>) -> Result<()> {
+        let organization_account = &mut ctx.accounts.organization_account;
+        let master = &mut ctx.accounts.master_account;
 
+        // Increment last_id in master
+        master.last_id += 1;
+
+
+        organization_account.organization_id = master.last_id;
+        organization_account.owner = ctx.accounts.owner_account.key();
+        organization_account.employee_count = 0;
+        Ok(())
+    }
+
+    pub fn create_payroll(
+        ctx: Context<CreatePayroll>,
+        _organization_id: u32,
+        amounts: Vec<u64>,
+        total_sol: u64,
+    ) -> Result<()> {
+        let organization_account = &mut ctx.accounts.organization_account;
+        let payroll_account = &mut ctx.accounts.payroll_account;
+        let owner = &ctx.accounts.owner_account;
+        let employees = &ctx.remaining_accounts;
         let total_amount: u64 = amounts.iter().sum();
-        require!(total_amount == sol_tokens, SolEarnError::IncorrectAllocations);
+        let employer_lamports: u64 = ctx.accounts.owner_account.lamports();
+
+        payroll_account.employer = ctx.accounts.owner_account.key();
+
+        require!(total_amount == total_sol, SolEarnError::IncorrectAllocations);
         require!(
             employees.len() as u16 == amounts.len() as u16,
             SolEarnError::InvalidEmployeeAmountCount
         );
-        require!(ctx.accounts.employer.is_signer, SolEarnError::NotSigner);
-
-        let employer_lamports: u64 = ctx.accounts.employer.lamports();
-        require!(employer_lamports > sol_tokens, SolEarnError::InsufficientSol);
+        require!(owner.is_signer, SolEarnError::NotSigner);
+        require!(employer_lamports > total_sol, SolEarnError::InsufficientSol);
+        
+        // LOG before transaction
         emit!(MyEvent{ data: employer_lamports });
 
-        let lmps: u64 = ctx.accounts.payout_account.to_account_info().lamports();
+        let lmps: u64 = payroll_account.to_account_info().lamports().clone();
         emit!(MyEvent{ data: lmps });
 
-        payroll_account.payroll_total = sol_tokens;
+        payroll_account.payroll_total = total_sol;
 
         let transfer_cpi_context = CpiContext::new(
             ctx.accounts.system_program.to_account_info(),
             Transfer {
-                from: ctx.accounts.employer.to_account_info(),
-                to: ctx.accounts.payout_account.to_account_info()
+                from: owner.to_account_info(),
+                to: payroll_account.to_account_info()
             },
         );
 
-        transfer(transfer_cpi_context, sol_tokens).unwrap();
+        transfer(transfer_cpi_context, total_sol).unwrap();
+
+        organization_account.last_payroll_id += 1;
+        payroll_account.payroll_id = organization_account.last_payroll_id;
 
         // ---- Transfer end -----//
 
-        employer_account.employee_count = employees.len() as u16;
+        organization_account.employee_count = employees.len() as u16;
 
         for (i, employee) in employees.iter().enumerate() {
             payroll_account.employee_addresses.push(employee.key());
@@ -79,51 +96,62 @@ mod solearn_contract {
         }
         
         //---- LOGS ----// 
-        let lmps_after: u64 = ctx.accounts.payout_account.to_account_info().lamports();
+        let lmps_after: u64 = ctx.accounts.payroll_account.to_account_info().lamports();
         emit!(MyEvent{ data: lmps_after });
 
-        let employer_lamports: u64 = ctx.accounts.employer.lamports();
+        let employer_lamports: u64 = ctx.accounts.owner_account.lamports();
         emit!(MyEvent{ data: employer_lamports });
         // ---- LOGS end -----//
 
         Ok(())
     }
 
-    pub fn payout(ctx: Context<Payout>, instruction_data: u8) -> Result<()> {
-        let (pda, _bump) = Pubkey::find_program_address(&[b"SOLEARN_PAYOUT_ACCOUNT"], ctx.program_id);
-        let bump_seed = instruction_data;
-
-        let employer_account = &mut ctx.accounts.employer_account;
+    pub fn initialize_payout(ctx: Context<Payout>, _organization_id: u32, _payroll_id: u32) -> Result<()> {
+        let organization_account = &mut ctx.accounts.organization_account;
         let payroll_account = &mut ctx.accounts.payroll_account;
+        let employee_account = &ctx.accounts.employee_account;
 
-        require!(payroll_account.employer.key() == ctx.accounts.employer.key(), SolEarnError::Unauthorized);
-        require!(payroll_account.employee_addresses.len() as u16 == employer_account.employee_count, SolEarnError::InvalidEmployeePayrollCount);
+        require!(payroll_account.employer.key() == organization_account.owner.key(), SolEarnError::Unauthorized);
+        require!(payroll_account.employee_addresses.len() as u16 == organization_account.employee_count, SolEarnError::InvalidEmployeePayrollCount);
+        require!(payroll_account.is_payroll_complete == false, SolEarnError::PayrollInactive);
 
-        require!(payroll_account.employee_addresses.contains(&ctx.accounts.employee_account.key()), SolEarnError::NotMember);
+        require!(payroll_account.employee_addresses.contains(&employee_account.key()), SolEarnError::NotMember);
 
         let position = payroll_account.employee_addresses.iter().position(|&x| x == ctx.accounts.employee_account.key()).unwrap();
 
-        let lmps: u64 = ctx.accounts.payout_account.to_account_info().lamports();
+        let lmps: u64 = payroll_account.to_account_info().lamports();
         emit!(MyEvent{ data: lmps });
 
-        let lmps: u64 = ctx.accounts.employee_account.to_account_info().lamports();
+        let lmps: u64 = employee_account.to_account_info().lamports();
         emit!(MyEvent{ data: lmps });
 
-        let seeds = vec![bump_seed];
-        let seeds = vec![b"SOLEARN_PAYOUT_ACCOUNT".as_ref(), seeds.as_slice()];
-        let seeds = vec![seeds.as_slice()];
-        let seeds = seeds.as_slice();
+        // let seeds = vec![bump_seed];
+        // let seeds = vec![b"SOLEARN_PAYOUT_ACCOUNT".as_ref(), seeds.as_slice()];
+        // let seeds = vec![seeds.as_slice()];
+        // let seeds = seeds.as_slice();
 
-        let transfer_cpi_context = CpiContext::new_with_signer(
-            ctx.accounts.payout_account.to_account_info(),
-            Transfer {
-                from: ctx.accounts.payout_account.to_account_info(),
-                to: ctx.accounts.employee_account.to_account_info(),
-            },
-            seeds
-        );
+        // let transfer_cpi_context = CpiContext::new_with_signer(
+        //     ctx.accounts.system_program.to_account_info(),
+        //     Transfer {
+        //         from: ctx.accounts.payout_account.to_account_info(),
+        //         to: ctx.accounts.employee_account.to_account_info(),
+        //     },
+        //     seeds
+        // );
 
-        transfer(transfer_cpi_context, payroll_account.employee_salaries[position]).unwrap();
+        // transfer(transfer_cpi_context, payroll_account.employee_salaries[position]).unwrap();
+
+        let salary = payroll_account.employee_salaries[position];
+        require!(salary != 0, SolEarnError::SalararyClaim);
+
+        // deduct emlpoyee salary from value in array -> should become 0
+        payroll_account.employee_salaries[position] -= salary;
+        payroll_account.payroll_total -= salary;
+
+        emit!(MyEventTest{ data: payroll_account.employee_salaries[position] });
+
+        **payroll_account.to_account_info().try_borrow_mut_lamports().unwrap() -= salary;
+        **employee_account.to_account_info().try_borrow_mut_lamports().unwrap() += salary;
         
         // let ix = &system_instruction::transfer(&ctx.accounts.payout_account.key, &ctx.accounts.employee_account.key, payroll_account.employee_salaries[position]);
         // invoke_signed(ix, &[
@@ -132,113 +160,113 @@ mod solearn_contract {
         //     ctx.accounts.system_program.to_account_info()
         // ], &[&[b"SOLEARN_PAYOUT_ACCOUNT", &[bump_seed]]])?;
 
-        let lmps: u64 = ctx.accounts.payout_account.to_account_info().lamports();
+        let remaining_payroll_balance = payroll_account.payroll_total;
+
+        if remaining_payroll_balance == 0 {
+            payroll_account.is_payroll_complete = true;
+        }
+
+        let lmps: u64 = payroll_account.to_account_info().lamports();
         emit!(MyEvent{ data: lmps });
 
-        let lmps: u64 = ctx.accounts.employee_account.to_account_info().lamports();
+        let lmps: u64 = employee_account.to_account_info().lamports();
         emit!(MyEvent{ data: lmps });
-
-        // **ctx.accounts.payout_account.try_borrow_mut_lamports()? -= payroll_account.employee_salaries[position];
-        // **ctx.accounts.employee_account.try_borrow_mut_lamports()? += payroll_account.employee_salaries[position];
-
-        Ok(())
-    }
-
-    pub fn assign_authority_to_pda(ctx: Context<AssignAuthorityToPDA>) -> Result<()> {
-        let (pda, _bump) = Pubkey::find_program_address(&[b"solearnpda"], ctx.program_id);
-
-        let cpi_accounts = SetAuthority {
-            current_authority: ctx.accounts.current_authority_signer.to_account_info(),
-            account_or_mint: ctx.accounts.program_pubkey.to_account_info()
-        };
-
-        token::set_authority(CpiContext::new(ctx.accounts.token_program.clone(), cpi_accounts), AuthorityType::AccountOwner, Some(pda))?;
 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction()]
 pub struct Initialize<'info> {
-    #[account(mut)]
-    pub employer: Signer<'info>,
-
     #[account(
         init, 
-        payer = employer, 
-        space = 8 + std::mem::size_of::<Organization>(), 
-        seeds = [EMPLOYER_TAG, employer.key().as_ref()], 
+        payer = authority, 
+        space = 8 + std::mem::size_of::<Master>(), 
+        seeds = [MASTER_TAG],
         bump
     )]
-    pub organization_account: Box<Account<'info, Organization>>,
+    pub master_account: Box<Account<'info, Master>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
 
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction()]
-pub struct AddEmployees<'info> {
-    #[account(mut)]
-    pub employer: Signer<'info>,
-
-    #[account(mut, has_one = employer)]
-    pub organization_account: Box<Account<'info, Organization>>,
-
+pub struct CreateOrganzation<'info> {
     #[account(
         init,
-        seeds = [EMPLOYEES_TAG, employer.key().as_ref()],
+        payer = owner_account,
+        space = 8 + std::mem::size_of::<Organization>(),
+        seeds = [ORGANIZATION_TAG, &(master_account.last_id + 1).to_le_bytes()],
         bump,
-        payer = employer,
+    )]
+    pub organization_account: Account<'info, Organization>,
+
+    #[account(
+        mut,
+        seeds = [MASTER_TAG],
+        bump
+    )]
+    pub master_account: Account<'info, Master>,
+
+    #[account(mut)]
+    pub owner_account: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(organization_id: u32)]
+pub struct CreatePayroll<'info> {
+    #[account(
+        init,
+        seeds = [PAYROLL_TAG, owner_account.key().as_ref(), &(organization_account.last_payroll_id + 1).to_le_bytes()],
+        bump,
+        payer = owner_account,
         space = std::mem::size_of::<Payroll>() + (10 * (std::mem::size_of::<Pubkey>() + std::mem::size_of::<u64>())) + 8,
     )]
     pub payroll_account: Box<Account<'info, Payroll>>,
 
-    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(
+        mut,
+        seeds = [ORGANIZATION_TAG, &organization_id.to_le_bytes()],
+        bump
+     )]
+    pub organization_account: Box<Account<'info, Organization>>,
+
     #[account(mut)]
-    pub payout_account: AccountInfo<'info>,
+    pub owner_account: Signer<'info>,
 
     pub system_program: Program<'info, System>,
-    // pub token_program: Program<'info, TokenAccount>,
 }
 
 #[derive(Accounts)]
-#[instruction()]
+#[instruction(organization_id: u32, payroll_id: u32)]
 pub struct Payout<'info> {
-    // #[account(mut)]
-    // pub pda: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [ORGANIZATION_TAG, &organization_id.to_le_bytes()],
+        bump
+    )]
+    pub organization_account: Box<Account<'info, Organization>>,
 
-    /// CHECK: This is not dangerous because we don't read or write from this account
-    #[account(mut)]
-    pub employer: AccountInfo<'info>,
-
-    #[account(mut)]
-    pub employer_account: Box<Account<'info, Organization>>,
-
-    /// CHECK: This is not dangerous because we don't read or write from this account
-    #[account(mut)]
-    pub payout_account: AccountInfo<'info>,
-
-    #[account(mut)]
+    #[account(
+        mut,
+        seeds = [PAYROLL_TAG, owner_account.key().as_ref(), &payroll_id.to_le_bytes()],
+        bump
+    )]
     pub payroll_account: Box<Account<'info, Payroll>>,
 
     /// CHECK: This is not dangerous because we don't read or write from this account
     #[account(mut)]
-    employee_account: AccountInfo<'info>,
+    pub owner_account: AccountInfo<'info>,
 
+    #[account(mut)]
+    employee_account: Signer<'info>,
 
     pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct AssignAuthorityToPDA<'info> {
-    /// CHECK: This is not dangerous because we don't read or write from this account
-    current_authority_signer: Signer<'info>,
-    /// CHECK: This is not dangerous because we don't read or write from this account
-    #[account(mut)]
-    pub program_pubkey: AccountInfo<'info>,
-    /// CHECK: This is not dangerous because we don't read or write from this account
-    pub token_program: AccountInfo<'info>,
 }
 
 #[event]
@@ -248,5 +276,5 @@ pub struct MyEvent {
 
 #[event]
 pub struct MyEventTest {
-    pub data: u16,
+    pub data: u64,
 }
